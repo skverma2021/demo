8d40c6bd1ba7dca4150bf00d30fd464bf1ab1370
SK Verma <s.k.verma@live.in>
2024-08-02 18:38:45 +0530

    2Aug24-2

--- frontend/src/demo/Demo.jsx
+++ frontend/src/demo/Demo.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
-// Alternative-I
 import { Link } from 'react-router-dom';
-
-// Alternative-II
-import Welcome from './Welcome2';
-import Calculate from './Calculate';
-import Series from './Series';
 
 export const Demo = () => {
   return (
     <div>
       <h1>Welcome to Demo</h1>
-      {/* Alternative-I */}
       <ul>
         <li>
           <Link to={`./welcome`}>Welcome</Link>
@@ -24,11 +17,6 @@ export const Demo = () => {
           <Link to={`./series`}>Series</Link>
         </li>
       </ul>
-
-      {/* Alternative-II */}
-      {/* <Welcome /> */}
-      {/* <Calculate /> */}
-      {/* <Series /> */}
     </div>
   );
 };
