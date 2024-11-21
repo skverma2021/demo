68b86021c6754d3d1e6369cfe9f4c6fe191d5b0c
skverma2021 <s.k.verma@live.in>
2024-11-21 08:01:07 +0530

    21Nov24-1

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 const Home = () => {
   return (
-    <>
-      <div style={{ width: '100%', height: '100vh' }}>
-        <h1 style={{ color: 'magenta', textAlign: 'center' }}>
+      <div style={{ width: '100%', height: '100vh', textAlign:'left' }}>
+        <h1 style={{ color: 'magenta' }}>
           SERN Tutorial - DEMO
         </h1>
-        <h5 style={{ color: 'magenta', textAlign: 'center' }}>
-          Building an MIS with SERN
+        <h5 style={{ color: 'magenta' }}>
+          Project: Basics of APIs and React
         </h5>
-        <p>In this Demo we create three utilities</p>
+        <p>An image slider and three utility components</p>
         <ul>
-          <li>a Welcome component to compute age of a person</li>
-          <li>a Calculator component to performs simple arithmetic</li>
-          <li>a Series component that computes sum,average etc.</li>
+          <li><b>Welcome</b>: computes age of a person</li>
+          <li><b>Calculator</b>: performs simple arithmetic</li>
+          <li><b>Series</b>: computes sum,average etc.</li>
         </ul>
       </div>
-    </>
   );
 };
 
